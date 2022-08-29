1ea468e95ffd8286bbe9afed5b78476f947f81e2
ShaniDan <smirabzalova88@gmail.com>
2022-08-28 21:57:08 -0700

    Algo

--- Algorithms/AlgorithmsApp.swift
+++ Algorithms/AlgorithmsApp.swift
@@ -17,4 +17,52 @@ struct AlgorithmsApp: App {
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
+    
+    func printLine() -> String {
+
+        let filename = "sowpods.txt"
+
+        var text: String
+
+        var myCounter: Int
+
+        print("hello world")
+        
+        guard let file = Bundle.main.url(forResource: filename, withExtension: "strings")
+
+        else {
+
+            fatalError("Couldn't find \(filename) in main bundle.")
+
+        }
+
+
+
+        do {
+
+            let contents = try String(contentsOf: file, encoding: String.Encoding.utf8 )
+
+            let lines = contents.split(separator:"\n")
+
+            print(contents)
+
+            print(lines)
+
+            myCounter = lines.count
+
+            print(myCounter)
+
+            text = String(myCounter)
+
+            } catch {
+
+                return (error.localizedDescription)
+
+            }
+
+            return text
+
+
+
+    }
 }
---
+++ problem1.swift
@@ -0,0 +1,9 @@
+//
+//  problem1.swift
+//  Algorithms
+//
+//  Created by Shakhnoza Mirabzalova on 8/19/22.
+//
+
+import Foundation
+
