a00ed1c23cf9189011e0a89c0bfed7c28361ab82
ShaniDan <smirabzalova88@gmail.com>
2022-09-18 19:16:32 -0700

    Words containing XYZ

--- MyPlayground.playground/Pages/2.WordsWithXYZ.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/2.WordsWithXYZ.xcplaygroundpage/Contents.swift
@@ -7,9 +7,9 @@ import UIKit
 func wordsContainingXYZ(filename:String) -> [String] {
     var allWords = [String]()
     var wordsWithXYZ = [String]()
-    if let startWordsPath = Bundle.main.path(forResource: filename, ofType: "txt") {
-        if let startWords = try? String(contentsOfFile: startWordsPath) {
-            allWords = startWords.components(separatedBy: "\n")
+    if let filePath = Bundle.main.path(forResource: filename, ofType: "txt") {
+        if let changeContentsToString = try? String(contentsOfFile: filePath) {
+            allWords = changeContentsToString.components(separatedBy: "\n")
             for word in allWords {
                 if word.contains("X")
                     && word.contains ("Y")
