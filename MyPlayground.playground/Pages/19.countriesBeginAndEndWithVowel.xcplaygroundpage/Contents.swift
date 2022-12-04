253f3f248009b46ae382b5b8c827a57945e1b66b
ShaniDan <smirabzalova88@gmail.com>
2022-12-04 12:58:55 -0800

    Countries begin and end with vowel

---
+++ MyPlayground.playground/Pages/19.countriesBeginAndEndWithVowel.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+// What countries both begin and end with a vowel?
+// array of countries
+// array of countries that begin and end with a vowel
+
+import Foundation
+
+func countriesBeginAndEndWithVowel() -> [String] {
+    var allCountries = readTxt(filename: "countries")
+    var countriesNameBeginAndEndVowel = [String]()
+    
+    for country in allCountries {
+        
+        if country.hasPrefix("A")
+            || country.hasPrefix("E")
+            || country.hasPrefix("I")
+            || country.hasPrefix("O")
+            || country.hasPrefix("U")
+        {
+            if country.hasSuffix("a")
+                || country.hasSuffix("e")
+                || country.hasSuffix("i")
+                || country.hasSuffix("o")
+                || country.hasSuffix("u") {
+                countriesNameBeginAndEndVowel.append(country)
+            }
+        }
+    }
+    return countriesNameBeginAndEndVowel
+}
+print(countriesBeginAndEndWithVowel())
+
