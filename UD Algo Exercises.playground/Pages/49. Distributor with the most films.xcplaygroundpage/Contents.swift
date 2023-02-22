3dc8d7cf44ebe00df3ccc8e724a01d8076733153
ShaniDan <smirabzalova88@gmail.com>
2023-02-22 10:18:14 -0800

    solution 49

---
+++ UD Algo Exercises.playground/Pages/49. Distributor with the most films.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+// What distributor has the most films on this list?
+
+import Foundation
+
+func solution() -> Optional<(key: String, value: Int)> {
+    var topMovies = readCsv(name: "top_movies").filter{!$0.isEmpty}
+    var dictDistributor = [String: Int]()
+    for line in topMovies {
+        var arrayLine = line.components(separatedBy: ",")
+        var distributor = arrayLine[1].trimmingCharacters(in: .whitespacesAndNewlines)
+        dictDistributor [distributor, default: 0] += 1
+    }
+    print(dictDistributor)
+    let result = dictDistributor.max { $0.value < $1.value }
+    return result
+}
+print(solution() ?? 0 as Any)
