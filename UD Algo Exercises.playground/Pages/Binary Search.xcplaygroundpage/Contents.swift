b63b4500d5fd7093dcf7d0ee6e032be1fa967b02
ShaniDan <smirabzalova88@gmail.com>
2023-04-18 14:23:53 -0700

    Binary search notes

---
+++ UD Algo Exercises.playground/Pages/Binary Search.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+//: [Previous](@previous)
+
+import Foundation
+// O(logn) - quadralogarithmic
+func binary() -> Bool {
+    var array = [1, 2, 3, 4, 5]
+    
+    for i in array {
+        if i == 3 {
+            
+        }
+    }
+    return false
+}
+print(binary())
